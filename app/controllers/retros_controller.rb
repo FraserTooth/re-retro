5932173f1fedeba981d18efe1aa50880c6aee911
Fraser Tooth <frasertooth@gmail.com>
2020-03-03 17:21:03 +0900

    Completed Update Views for Article

--- app/controllers/retros_controller.rb
+++ app/controllers/retros_controller.rb
@@ -1,30 +1,45 @@
+# frozen_string_literal: true
+
 class RetrosController < ApplicationController
-    def index
-        @retro = Retro.all
+  def index
+    @retro = Retro.all
+  end
+
+  def show
+    @retro = Retro.find(params[:id])
+  end
+
+  def new
+    @retro = Retro.new
+  end
+
+  def edit
+    @retro = Retro.find(params[:id])
+  end
+
+  def create
+    @retro = Retro.new(retro_params)
+
+    if @retro.save
+      redirect_to @retro
+    else
+      render 'new'
     end
-    
-    def show
-        @retro = Retro.find(params[:id])
+  end
+
+  def update
+    @retro = Retro.find(params[:id])
+
+    if @retro.update(retro_params)
+      redirect_to @retro
+    else
+      render 'edit'
     end
+  end
 
+  private
 
-    def new
-        @retro = Retro.new
-    end
-
-    def create
-
-        @retro = Retro.new(retro_params)
-
-        if @retro.save
-            redirect_to @retro
-        else
-            render 'new'
-        end
-    end
-
-    private
-        def retro_params
-            params.require(:retro).permit(:facilitator, :date, :team, :introduction)
-        end
+  def retro_params
+    params.require(:retro).permit(:facilitator, :date, :team, :introduction)
+  end
 end
--- app/models/retro.rb
+++ app/models/retro.rb
@@ -1,5 +1,7 @@
+# frozen_string_literal: true
+
 class Retro < ApplicationRecord
-    validates :facilitator, presence: true, length: {minimum: 5}
-    validates :date, presence: true
-    validates :team, presence: true
+  validates :facilitator, presence: true, length: { minimum: 2 }
+  validates :date, presence: true
+  validates :team, presence: true
 end
